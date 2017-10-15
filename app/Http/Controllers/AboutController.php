18a0416a09bbbe4eecb814ed9ffb9cc7c0f468dc
nini <poikl15397@gmail.com>
2017-10-15 16:25:17 +0800

    debug

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -8,6 +8,6 @@ class AboutController extends Controller
 {
     public function index()
     {
-        return 'index';
+        return view('about');
     }
 }
--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -8,6 +8,6 @@ class NewsController extends Controller
 {
     public function index()
     {
-        return 'index';
+        return view('news');
     }
 }
